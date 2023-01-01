bc4fa05b68ff877e48e1174054e70bf78bc91333
Tedcharlesbrown <52015952+Tedcharlesbrown@users.noreply.github.com>
2022-12-31 17:34:36 -0800

    Update osc_to_barco.py

--- osc_to_barco.py
+++ osc_to_barco.py
@@ -27,8 +27,8 @@ def convert_layer_id(n: int) -> int:
     else:
         return n
 
-def send_barco_xml_threaded(url: str, screenDest_id: int, layer_id: int, opacity: int):
-  threading.Thread(target=send_barco_xml, args=(url, screenDest_id, layer_id, opacity)).start()
+def send_barco_xml_threaded(url: str, screenDest_id: int, layers: list, opacity: int):
+  threading.Thread(target=send_barco_xml, args=(url, screenDest_id, layers, opacity)).start()
   # 50 = 0.3
   # 25 = 0.5
   # 15 = 0.75
@@ -36,88 +36,56 @@ def send_barco_xml_threaded(url: str, screenDest_id: int, layer_id: int, opacity
   sleep_time = 0.1 / 11.25
   sleep(sleep_time)
 
-def send_barco_xml(url: str, screenDest_id: int, layer_id: int, opacity: int):
-    layer_id = convert_layer_id(layer_id)
-    # <System id="0" GUID="a05950e12070-00296c" OPID="-1">
-    xml_data = f"""
+def send_barco_xml(url: str, screenDest_id: int, layers: list, opacity: int):
+    xml_data =f"""
     <System id="0" GUID="0" OPID="0">
     <DestMgr id="0">
-        <ScreenDestCol id="0">
-        <ScreenDest id="{screenDest_id}">
-            <LayerCollection id="0">
-            <Layer id="{layer_id}">
-                <LayerCfg id="0">
-                <LayerState id="0">
-                    <PIP id="0">
-                    <Opacity>{opacity}</Opacity>
-                    </PIP>
-                </LayerState>
-                </LayerCfg>
-            </Layer>
-            </LayerCollection>
-        </ScreenDest>
-        </ScreenDestCol>
+    <ScreenDestCol id="0">
+    <ScreenDest id="{screenDest_id}">
+    <LayerCollection id="0">
+    """
+
+    xml_suffix = f"""
+    </LayerCollection>
+    </ScreenDest>
+    </ScreenDestCol>
     </DestMgr>
     </System>
     """
-    print(f"Sending: DESTINATION:{screenDest_id}, LAYER:{layer_id}, OPACITY:{opacity}")
 
-    # ------------------------------------ TCP ----------------------------------- #
+    for layer in range(len(layers)):
 
-    # Create a TCP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        layer = convert_layer_id(layer)
 
-    # Connect to the specified host and port
-    s.connect((url, 9876))
+        xml_layer = f"""
+        <Layer id="{layer}">
+        <LayerCfg id="0">
+        <LayerState id="0">
+        <PIP id="0">
+        <Opacity>{opacity}</Opacity>
+        </PIP>
+        </LayerState>
+        </LayerCfg>
+        </Layer>
+        """
 
-    # Send the XML data over the socket
-    s.sendall(xml_data.encode())
+        
 
-    # Close the socket
-    s.close()
 
-    # ------------------------------------ UDP ----------------------------------- #
+
+    # print(f"Sending: DESTINATION:{screenDest_id}, LAYER:{layer_id}, OPACITY:{opacity}")
+
+    # # Create a TCP socket
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # # Connect to the specified host and port
-    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # s.connect((url, 9876))
-    
+
     # # Send the XML data over the socket
     # s.sendall(xml_data.encode())
-    
+
     # # Close the socket
     # s.close()
-
-    # ------------------------------- HTTP REQUEST ------------------------------- #
-
-    # headers = {'Content-Type': 'application/xml'}
-    # url = f'http://{url}:9876'
-
-    # try:
-    #     requests.post(url, data=xml_data, headers=headers, timeout=1.5)
-    # except:
-    #     pass
-
-
-
-# #Zero Based, Screen Destination, Sequential (0,1,2,3)
-# destination = 0
-# #Zero Based, Layer ID, iterates by 2 (0,2,4,6)
-# layer = 0
-
-# opacity = 100
-# opacity = random.randint(0, 100)
-
-# time_start = time.time()
-
-# print(f"START")
-
-# for i in range(0,101):
-#     send_barco_xml_threaded("127.0.0.1",destination,1,i)
-#     # sleep(0.05)
-#     # send_barco_xml("127.0.0.1",destination,1,i)
-
-# print(f"FINISHED IN: {(time.time() - time_start):.2f}")
 
 # ---------------------------------------------------------------------------- #
 #                                      OSC                                     #
@@ -131,17 +99,29 @@ def remap(input_value: float, in_min: float, in_max: float, out_min: float, out_
     output_value = input_value_scaled * (out_max - out_min) + out_min
     return output_value
 
-def parse_layer(address: str) -> list:
-    
-    if address[:7] == "/barco/":
-        
-        address_list = address[7:].split("/")
-        return address_list
+def get_pattern(address: str, x: int) -> str:
+    address_list = address.split('/')
+    address_list.pop(0)
 
-def handle_args_1(address, x):
-    dest = int(parse_layer(address)[0])
-    layer = int(parse_layer(address)[1])
-    send_barco_xml_threaded(BARCO_IP_ADDRESS,dest,layer,int(remap(x,0,1,0,100)))
+    if x > len(address_list):
+        return False
+    else:
+        return address_list[x]
+
+def handle_args_1(address, value):
+    # print(get_pattern(address,0).lower())
+    top = get_pattern(address,0).lower()
+    if top == "barco":
+        layers = get_pattern(address,1)
+        layers = layers.split(',')
+        # print("TEST")
+        print(layers,value)
+        # print(get_pattern(address,layer))
+
+    # if address[:7] == "/barco/":
+    # dest = int(parse_layer(address)[0])
+    # layer = int(parse_layer(address)[1])
+    # send_barco_xml_threaded(BARCO_IP_ADDRESS,dest,layer,int(remap(value,0,1,0,100)))
 
 
 # ---------------------------------------------------------------------------- #
@@ -149,33 +129,34 @@ def handle_args_1(address, x):
 # ---------------------------------------------------------------------------- #
 
 def main():
-    global BARCO_IP_ADDRESS
+    # global BARCO_IP_ADDRESS
 
-    PORT_NUMBER = input("What port should be used to receive OSC? ")
-    if PORT_NUMBER.isdigit():
-        PORT_NUMBER = int(PORT_NUMBER)
-    else:
-        print("INVALID PORT NUMBER")
-        main()
-    BARCO_IP_ADDRESS = input("What is the IP address of the Barco Device? ")
-    if isinstance(BARCO_IP_ADDRESS,str):
-        print(f"Connecting to Barco Device at: {BARCO_IP_ADDRESS}")
-        try:
-            ping(BARCO_IP_ADDRESS)
-        except:
-            print("COULD NOT CONNECT TO BARCO DEVICE")
-            main()
+    # PORT_NUMBER = input("What port should be used to receive OSC? ")
+    # if PORT_NUMBER.isdigit():
+    #     PORT_NUMBER = int(PORT_NUMBER)
+    # else:
+    #     print("INVALID PORT NUMBER")
+    #     main()
+    # BARCO_IP_ADDRESS = input("What is the IP address of the Barco Device? ")
+    # if isinstance(BARCO_IP_ADDRESS,str):
+    #     print(f"Connecting to Barco Device at: {BARCO_IP_ADDRESS}")
+    #     try:
+    #         ping(BARCO_IP_ADDRESS)
+    #     except:
+    #         print("COULD NOT CONNECT TO BARCO DEVICE")
+    #         main()
         
 
-    print("PING SUCCESSFUL")
-    print("---------------")
-    print()
+    # print("PING SUCCESSFUL")
+    # print("---------------")
+    # print()
 
     # Start the system.b
     osc_startup()
 
     # Make server channels to receive packets.
-    osc_udp_server("0.0.0.0", PORT_NUMBER, "anotherserver")
+    # osc_udp_server("0.0.0.0", PORT_NUMBER, "anotherserver")
+    osc_udp_server("0.0.0.0", 7400, "anotherserver")
 
 
     osc_method("/*", handle_args_1, argscheme=osm.OSCARG_ADDRESS + osm.OSCARG_DATAUNPACK)
